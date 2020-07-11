2b072f562fffc0bf5a3d0ebd7bb60dc755acefb1
Mark <marknjensen@outlook.com>
2020-07-11 22:04:02 +0200

    led

--- led.py
+++ led.py
@@ -3,5 +3,9 @@ import sys
 
 led = LED(17)
 
-if sys.argv[1] == "ON": led.on()
-if sys.argv[1] == "OFF": led.off()
+if sys.argv[1] == "ON": 
+    led.on()
+    print("LED ON")
+if sys.argv[1] == "OFF": 
+    led.off()
+    rint("LED ON")
