f75919f00b97705aa345cf71fe02ddc122684cf2
Mark <marknjensen@outlook.com>
2020-07-13 00:33:29 +0200

    express

--- server/routes/mark.js
+++ server/routes/mark.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-    res.render("script", {title: "Mark", message: "Hey there"});
+    res.render("mark", {title: "Mark", message: "Hey there"});
 });
 
 module.exports = router;
