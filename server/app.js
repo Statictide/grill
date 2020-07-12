f38b6b8d51f37932ec7bd8fdec7772a89b569db8
Mark <marknjensen@outlook.com>
2020-07-12 19:00:56 +0200

    express

--- server/app.js
+++ server/app.js
@@ -1,11 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-//var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var scriptRouter = require('./routes/mark');
 
 var app = express();
 
@@ -13,14 +12,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mark', scriptRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
