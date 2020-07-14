fc30dc9a01c4b42f70e5dfba32283cc13b499d6e
Mark <marknjensen@outlook.com>
2020-07-14 17:36:18 +0200

    Update blink.py

--- pi/blink.py
+++ pi/blink.py
@@ -3,8 +3,11 @@ import time
 
 led = LED(17)
 
-while True:    
+while True:
+    print("ON")
     led.on()
     time.sleep(1)
+
+    print("OFF")
     led.off()
-    time.sleep()
+    time.sleep(1)
