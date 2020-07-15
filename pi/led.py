78d22ece0368f2ca7f68c2d755873c78a59be0c4
Mark <marknjensen@outlook.com>
2020-07-15 17:33:09 +0200

    migrate to pug

--- pi/led.py
+++ pi/led.py
@@ -2,15 +2,10 @@ from gpiozero import LED
 import sys
 import time
 
-if(len(sys.argv)) <= 1: exit()  #At least 1 argument
 gpio_pin : int = int(sys.argv[1])
 
-led = LED(gpio_pin)
+led = LED(2)
 led.on()
 
-time.sleep(4)
-
-led = LED(18)
-led.on()
 
 time.sleep(4)
