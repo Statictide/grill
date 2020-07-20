4782be55338a1b66ab068037f6a4c98a1ead1880
Mark <marknjensen@outlook.com>
2020-07-20 14:16:24 +0200

    cookie-parser

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cookieParser = require('cookie-parser');
 
 var grillRouter = require('./routes/grill');
 var idRouter = require('./routes/id');
@@ -13,6 +14,7 @@ app.set('view engine', 'pug');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
