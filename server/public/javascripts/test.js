78456e99f3a489a5797e5c4c855b647f78291f5f
Mark <marknjensen@outlook.com>
2020-07-21 12:40:17 +0200

    Update test.js

--- server/public/javascripts/test.js
+++ server/public/javascripts/test.js
@@ -34,24 +34,3 @@ function onConnectionLost(responseObject) {
 function onMessageArrived(message) {
   console.log(message.destinationName + ":" + message.payloadString);
 }
-
-
-
-
-var stripe = Stripe('pk_test_51H4hdCKXVCnO2pJVsgZiUI6FRFLuypE6LfiNdOKPMNqdnScPRFGYgNBXpX71792TcgPQdby3VVHeQ65od6mKiTQ100ctrEddPA');
-
-var response = fetch('/id')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(responseJson) {
-  var sessionID = responseJson.session_id;
-  // Call stripe.redirectToCheckout() with the Session ID.
-
-  var checkoutButton = document.getElementById('checkout-button');
-
-  checkoutButton.addEventListener('click', function() {
-    stripe.redirectToCheckout({sessionId: sessionID})
-      .then(function (result) { /* Display error on network failure */ });
-  });
-});
